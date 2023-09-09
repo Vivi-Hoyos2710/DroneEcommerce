619a5f6f6b90c1481bb0902a96ce67ccb865c35a
jdramirezl <juliandavid.ramirez@mercadolibre.com.co>
2023-09-08 20:37:55 -0500

    Implemented create and edit for admin

--- src/resources/views/admin/product/edit.blade.php
+++ src/resources/views/admin/product/edit.blade.php
@@ -10,61 +10,47 @@
         <div class="bg-white shadow-xl p-10 flex flex-col gap-4 text-sm">
             <!-- Creation form for the product -->
             <div class="flex flex-col gap-4">
-                <form action="{{ route('admin.product.store') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('admin.product.update', $viewData['product']->getId()) }}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    @foreach ($errors->all() as $error)
+                        <p class="text-red-500">{{ $error }}</p>
+                    @endforeach
+
                     <div class="flex flex-col gap-4">
                         <label for="name">Name</label>
-                        <input type="text" name="name" id="name" class="border border-gray-300 p-2 rounded-lg" placeholder="Name" value="{{ old('name') }}">
-                        @error('name')
-                        <p class="text-red-500">{{ $message }}</p>
-                        @enderror
+                        <input type="text" name="name" id="name" value="{{ $viewData['product']->getName() }}" class="border border-gray-300 p-2 rounded-lg">
                     </div>
-                        <div class="flex flex-col gap-4">
-                            <label for="price">Price</label>
-                            <input type="number" name="price" id="price" class="border border-gray-300 p-2 rounded-lg" placeholder="Price" value="{{ old('price') }}">
-                            @error('price')
-                            <p class="text-red-500">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div class="flex flex-col gap-4">
-                            <label for="description">Description</label>
-                            <input type="text" name="description" id="description" class="border border-gray-300 p-2 rounded-lg" placeholder="Description" value="{{ old('description') }}">
+                    <div class="flex flex-col gap-4">
+                        <label for="price">Price</label>
+                        <input type="number" name="price" id="price" value="{{ $viewData['product']->getPrice() }}" class="border border-gray-300 p-2 rounded-lg">
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <label for="description">Description</label>
+                        <textarea name="description" id="description" cols="30" rows="10" class="border border-gray-300 p-2 rounded-lg">{{ $viewData['product']->getDescription() }}</textarea>
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <label for="category">Category</label>
+                        <input type="text" name="category" id="category" value="{{ $viewData['product']->getCategory() }}" class="border border-gray-300 p-2 rounded-lg">
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <label for="size">Size</label>
+                        <input type="text" name="size" id="size" value="{{ $viewData['product']->getSize() }}" class="border border-gray-300 p-2 rounded-lg">
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <label for="brand">Brand</label>
+                        <input type="text" name="brand" id="brand" value="{{ $viewData['product']->getBrand() }}" class="border border-gray-300 p-2 rounded-lg">
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <label for="image">Image</label>
+                        <input type="file" name="image" id="image" class="border border-gray-300 p-2 rounded-lg">
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <button type="submit" class="bg-blue-500 text-white p-2 rounded-lg">Save</button>
+                    </div>
 
-                            @error('description')
-                            <p class="text-red-500">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div class="flex flex-col gap-4">
-                            <label for="image">Image</label>
-                            <input type="file" name="image" id="image" class="border border-gray-300 p-2 rounded-lg" placeholder="Image" value="{{ old('image') }}">
-                            @error('image')
-                            <p class="text-red-500">{{ $message }}</p>
-                            @enderror
-                        </div>
-                        <div class="flex flex-col gap-4">
-                            <label for="category">Category</label>
-                            <input type="text" name="category" id="category" class="border border-gray-300 p-2 rounded-lg" placeholder="Category" value="{{ old('category') }}">
-                            @error('category')
-                            <p class="text-red-500">{{ $message }}</p>
-                            @enderror
-
-                            </div>
-                            <div class="flex flex-col gap- 4">
-
-                                <label for="size">Size</label>
-                                <input type="text" name="size" id="size" class="border border-gray-300 p-2 rounded-lg" placeholder="Size" value="{{ old('size') }}">
-
-                                @error('size')
-                                <p class="text-red-500">{{ $message }}</p>
-                                @enderror
-                                </div>
-                                <div class="flex flex-col gap-4">
-                                    <label for="brand">Brand</label>
-                                    <input type="text" name="brand" id="brand" class="border border-gray-300 p-2 rounded-lg" placeholder="Brand" value="{{ old('brand') }}">
-                                    @error('brand')
-                                    <p class="text-red-500">{{ $message }}</p>
-                                    @enderror
-                                    </div>
+                </form>
+            </div>
+        </div>
                                     
     </div>
 </div>
