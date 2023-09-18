a6970a2c27cbe5483123a600f716c5b70cbdfd4a
Viviana Hoyos Sierra <vhoyosss@eafit.edu.co>
2023-09-18 17:45:34 -0500

    fixing merging

--- src/app/Http/Controllers/User/WishListController.php
+++ src/app/Http/Controllers/User/WishListController.php
@@ -12,7 +12,7 @@ use Illuminate\View\View;
 
 class WishListController extends Controller
 {
-    public function index(): View
+    
     public function index(): View
     {
         $viewData = [];
@@ -24,13 +24,6 @@ class WishListController extends Controller
         return view('user.wishList.index')->with('viewData', $viewData);
     }
 
-    public function delete(string $id, string $WishListId): RedirectResponse
-    {
-        $wishlist = WishList::find($WishListId);
-        $wishlist->products()->detach($id);
-
-        return redirect() -> route('wishlist.index');
-    }
     public function delete(string $id, string $WishListId): RedirectResponse
     {
         $wishlist = WishList::find($WishListId);
