cccb119bbaa1dde105dc47db9b7417f4e43535a4
jdramirezl <jdramirezl@users.noreply.github.com>
2023-11-12 21:04:13 +0000

    PHP Linting (Pint)

--- src/app/Http/Controllers/Api/ProductController.php
+++ src/app/Http/Controllers/Api/ProductController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DroneResource;
 use App\Http\Resources\ProductFormatResource;
-use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
-use App\Http\Resources\DroneResource;
 use Throwable;
 
 class ProductController extends Controller
@@ -15,30 +14,36 @@ class ProductController extends Controller
     public function allProducts(): JsonResponse
     {
         $products = new DroneResource(Product::all());
-        return response()->json($products,200);
+
+        return response()->json($products, 200);
     }
+
     public function allAccessories(): JsonResponse
     {
         $accesories = new DroneResource(Product::where('category', 'accessory')->get());
-        return response()->json($accesories,200);
+
+        return response()->json($accesories, 200);
     }
+
     public function allBases(): JsonResponse
     {
         $bases = new DroneResource(Product::where('category', 'base')->get());
-        return response()->json($bases,200);
+
+        return response()->json($bases, 200);
     }
+
     public function productById(string $id): JsonResponse
     {
         try {
-            
+
             $product = Product::findOrFail($id);
             $accesories = new ProductFormatResource($product);
-            return response()->json($accesories,200);
+
+            return response()->json($accesories, 200);
         } catch (Throwable $th) {
             dd($th);
+
             return response()->json(['message' => 'Product not found'], 404);
         }
     }
-
-
 }
--- src/resources/lang/es/registerView.php
+++ src/resources/lang/es/registerView.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 return [
     'title' => 'Registro',
-    'name'=> 'Nombre',
-    'email'=> 'Correo electrónico',
-    'username'=> 'Nombre de usuario',
-    'password'=> 'Contraseña',
-    'confirm'=> 'Confirmar contraseña',
-    'already'=> '¿Ya tienes una cuenta?',
-    'login_here'=>'Inicia sesión aquí',
+    'name' => 'Nombre',
+    'email' => 'Correo electrónico',
+    'username' => 'Nombre de usuario',
+    'password' => 'Contraseña',
+    'confirm' => 'Confirmar contraseña',
+    'already' => '¿Ya tienes una cuenta?',
+    'login_here' => 'Inicia sesión aquí',
 ];
