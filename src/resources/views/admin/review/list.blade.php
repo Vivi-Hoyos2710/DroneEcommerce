3a46fe798d7351d8451f2fd13722f688728d0b9c
Viviana Hoyos <vhoyoss@eafit.edu.co>
2023-11-13 11:11:19 -0500

    Two sections in reviews

--- src/resources/views/admin/review/list.blade.php
+++ src/resources/views/admin/review/list.blade.php
@@ -3,12 +3,28 @@
 @section('content')
     <div class="relative overflow-x-auto">
         <div class="container mx-auto p-4">
-            <h1 class="text-2xl font-semibold mb-4">{{ __('product.reviewTitle') }} Accepted</h1>
+
+            @if ($viewData['type'] === 'rejected')
+        
+                <h1 class="dark:text-white text-2xl font-semibold mb-4">
+                    {{ __('review.rr') }}
+                </h1>
+            @else
+                <h1 class="dark:text-white text-2xl font-semibold mb-4">
+                    {{ __('review.ar') }}
+                </h1>
+            @endif
+
 
 
             @if (session('rejected'))
                 <div class="bg-red-200 text-red-800 border border-red-400 rounded p-2 mb-4">
                     {{ session('rejected') }}
+                </div>
+            @endif
+            @if (session('success'))
+                <div class="bg-green-200 text-green-800 border border-green-400 rounded p-2 mb-4">
+                    {{ session('success') }}
                 </div>
             @endif
             @foreach ($viewData['reviews'] as $review)
@@ -20,13 +36,25 @@
                         <p class="font-semibold text-lg"> {{ __('review.rating') }} {{ $review->getRating() }}</p>
                         <p class="text-gray-600">{{ $review->getDescription() }}</p>
                         <p class="text-gray-600">{{ $review->getCreatedAt() }}</p>
-                        <form method="POST" action="{{ route('admin.reviews.reject', $review->id) }}" class="mt-2">
-                            @csrf
-                            <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
-                                {{ __('review.reject') }}
-                            </button>
-                        </form>
-                        <form method="POST" action="{{ route('admin.reviews.delete', $review->id) }}" class="mt-2">
+                        @if ($viewData['type'] == 'rejected')
+                            <form method="POST" action="{{ route('admin.reviews.accept', $review->getId()) }}"
+                                class="mt-2">
+                                @csrf
+                                <button type="submit" class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
+                                    {{ __('review.accept') }}
+                                </button>
+                            </form>
+                        @else
+                            <form method="POST" action="{{ route('admin.reviews.reject', $review->getId()) }}"
+                                class="mt-2">
+                                @csrf
+                                <button type="submit" class="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600">
+                                    {{ __('review.reject') }}
+                                </button>
+                            </form>
+                        @endif
+
+                        <form method="POST" action="{{ route('admin.reviews.delete', $review->getId()) }}" class="mt-2">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">
